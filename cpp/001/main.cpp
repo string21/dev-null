7117bda044a6d1add37d4830f9bdedb250e698d4
gdroid <gdroid0021@gmail.com>
2025-10-19 06:00:37 +0800

    tried to define terms using own words

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -4,3 +4,90 @@ int main () {
     std::cout << "Hello World!\n";
     return 0;
 }
+
+/*
+
+//
+this is a comment
+
+function
+resusable piece of code
+
+output
+output of the code
+
+compiler
+compiles source code to object code or machine code
+
+error
+something is wrong with the code or program
+
+linker
+combines object code and turn them (1, 2 or more onbject codes) into an executable
+
+command line
+the terminal
+
+executable
+the resulting app sort of that can be run in the OS
+
+main()
+the function that "the system" calls when you run a cpp program
+
+<<
+output operator it think, used with cout
+
+
+header file
+this is the #include <iostream> part
+
+program
+the thing you code lol
+
+compile-time
+when you compile
+
+cout
+standard output
+
+std
+the standard library of c++
+
+object code
+the resulting machine code when you compile your source code, does not include comments
+
+comment
+the why's and how's of your code, logic, etc.
+
+import
+new in c++
+
+library
+something you import to use additonal facilities
+
+module
+similar to library
+
+bug
+error
+
+C++
+programming lang
+
+IDE
+interactive or integrated development environment
+
+source code
+the code that you write using the c++ syntax, human-readable
+
+statement
+a single instruction -- for now it's anything that ends with ; or anything that ends with }
+
+#include
+used to include header files
+
+debugging
+act of finding errors
+
+
+ */
