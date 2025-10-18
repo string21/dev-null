629b8cbe4d8082dffc9997edcc394f529f37dac6
gdroid <gdroid0021@gmail.com>
2025-10-19 06:31:33 +0800

    completed cin with observations

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -8,3 +8,12 @@ int main () {
     cin >> firstName;
     cout << "hello, " << firstName;
 }
+
+// what is definition -- it is when you introduce a new name for a variable in your program and set aside memory for it
+// cin refers to standard input stream -- stands for character input
+// cin is defined in the standard library
+// when you cin and press enter, the new ine will not stored when you hit enter
+
+
+
+
