4f0face33e858e66be5c1d4d13e2b777ae9952b2
gdroid <gdroid0021@gmail.com>
2025-10-19 06:05:02 +0800

    two lines of text

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 int main () {
-    std::cout << "Hello World!\n";
-    return 0;
+    std::cout << "Hello, programming!\n";
+    std::cout << "Here we go!";
+    // return 0;
+    // still works even if i remove the return on main
 }
 
 /*
