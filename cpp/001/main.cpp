fbc6cd4d7be2b7430c6c2e0f75a52c36633d0ea4
gdroid <gdroid0021@gmail.com>
2025-10-20 12:32:36 +0800

    miles to km converter

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -2,58 +2,16 @@
 
 using namespace  std;
 
-bool validateAge(int age);
-
 int main () {
 
-    cout << "\n\n********* some letter app *********\n\n";
-    string first_name;
-    cout << "Enter the name of the person you want to write to: ";
-    getline(cin, first_name);
+    cout << "miles to km converter....\n\n";
 
-    string another_friend_name;
-    cout << "Enter the name of another friend: ";
-    getline(cin, another_friend_name);
+    double miles = 0.00;
 
-    int age;
+    cout << "enter number to convert from miles to km: ";
+    cin >> miles;
 
-    do {
-        cout << "Enter age of your recipient: ";
-        cin >> age;
-        bool isValidAge = validateAge(age);
-        if (!isValidAge) {
-            cout << "you're kidding...";
-        } else {
-            break;
-        }
-    } while(true);
-
-    cout << "\nDear " << first_name << ",\n\n";
-
-    cout << "\tHow are you? I miss you.\n" << "Lorem ipsum dotor sit amet blah blah blah.\n";
-
-    cout << "Have you seen " << another_friend_name << " lately?\n";
-
-    cout << "I hear you just had a birthday and you are " << age << " years old.\n";
-
-    if (age < 12) {
-        cout << "Next year you will be " << age+1 << ".\n";
-    } else if (age == 17) {
-        cout << "Next year you will be able to vote.\n";
-    } else if (age > 70) {
-        cout << "Are you retired?\n";
-    }
-
-    cout << "\nYours sincerely,\n";
-    cout << "___________________\n";
-    cout << "___________________\n";
-    cout << "string21";
-
-    cout << "\n\n********* some letter app *********\n\n";
+    cout << "that is " << miles * 1.609 << " km.";
 
     return 0;
-}
-
-bool validateAge(int age) {
-    return age <=0 || age >= 110 ? false :  true; // too complex or not readable, use simpler if?
 }
