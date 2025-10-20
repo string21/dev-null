e184578d2cb0f868e64ba6cf4d71d81990e93647
gdroid <gdroid0021@gmail.com>
2025-10-20 10:22:32 +0800

    user-defined types vs c++ native types or even those in the standard library

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
+// for types we define, we capitalized the first letter to distinguish them from types defined in the standard library
 
-using namespace std;
-
-int main() {
-    string s = "Goodbye, cruel world! ";
-   cout << s << '\n';
-    return 0;
-}
-
-// you can use facilities found in the standard library (so if you think about it, those are not really keywords per se) but they are part of the standard library that you most likely will use..namespace
-
-// so it does not make sense to use them = bad practice -- so similar idea applies to any library or modules that you import, you should be aware of the names to avoid usign as your identifiers for variables, functions, etc etc
+// examples are string, int, double
+// user defines type examples: Car, Person, Something
