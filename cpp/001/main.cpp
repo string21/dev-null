e63245c57c1f51c2f915e03250741770759bad8b
gdroid <gdroid0021@gmail.com>
2025-10-20 11:34:45 +0800

    more about narrowing

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -1,24 +1,3 @@
-#include <iostream>
+// most compliers can warn about narrowing --- perharps i just need to adhere to those to avoid issues
 
-using namespace std;
-
-int main() {
-
-    double d = 0;
-
-    while (cin >> d) {
-        int i = d;
-        char c = i;
-
-        cout << "the original double is " << d << "\n";
-        cout << "double converted to int is " << i << "\n";
-        cout << "int value of char is " << c << "\n";
-
-    }
-
-    return 0;
-
-}
-
-
-// enabled warning in your IDE will help with type safety and other bugs  --- i think it is some kind of a -wall compiler option --- look into this
+// regardin initialization of variables, use {} or ={} if you want compile-time protections against narrowing
