b0f807828a64077e5a4833813316484d3a335069
gdroid <gdroid0021@gmail.com>
2025-10-20 10:06:30 +0800

    regarding keywords and naming identifiers

--- cpp/001/main.cpp
+++ cpp/001/main.cpp
@@ -7,3 +7,7 @@ int main() {
    cout << s << '\n';
     return 0;
 }
+
+// you can use facilities found in the standard library (so if you think about it, those are not really keywords per se) but they are part of the standard library that you most likely will use..namespace
+
+// so it does not make sense to use them = bad practice -- so similar idea applies to any library or modules that you import, you should be aware of the names to avoid usign as your identifiers for variables, functions, etc etc
