e08dc01c7e6cd3d247e984ebc1a9d9ae2143afb1
gdroid <gdroid0021@gmail.com>
2025-10-21 17:50:39 +0800

    integer values of alphabet

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
 using namespace std;
 
-constexpr double y = 1;
-constexpr double k = 2;
-constexpr double p = 3;
-
-double convertDollar(double dollar, char unit);
 
 int main () {
 
-    double dollar = 0;
-    char unit;
-    double result = 0;
+    int x = 100;
 
-    cout << "enter dollar to convert and unit separated by whitespace: ";
+    if (x == 100) {
+        cout << "1111";
+    } else if (x == 102) {
+        cout << "first else";
+    } else {
+        cout << "second else";
+    }
 
-    cin >> dollar >> unit;
-
-    result = convertDollar(dollar, unit);
-
-    if (result)
-        cout << dollar << " USD is " << result << unit;
-    else
-        cout << "check your input"
-
-
-
-    return 0;
-}
-
-double convertDollar(double dollar, char unit) {
-    if (unit == 'y')
-        return dollar * y;
-    else if (unit == 'k')
-        return dollar * k;
-    else if (unit == 'p')
-        return dollar * p;
     return 0;
 }
