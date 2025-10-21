aa5390f8855d1ab21d412523485fc89e8a3deec1
gdroid <gdroid0021@gmail.com>
2025-10-21 18:23:20 +0800

    blank

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
 int main () {
 
-    int i = 0;
 
-    while (i < 26   ) {
-
-        cout << char('a' + i) << "\t" << (int) char('a' + i) << "\n";
-
-        ++i;
-    }
 
     return 0;
 }
