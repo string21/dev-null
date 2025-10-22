cf2b5253dc71dd572721c367a8e847b4219ae2de
gdroid <gdroid0021@gmail.com>
2025-10-22 19:56:02 +0800

    distance between two cities

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -3,35 +3,59 @@
 using namespace std;
 
 /*
- * fix the median for even number of inputs "drill"
- */
+* Read a sequence of double values into a vector.
+* Think of each value as the distance between two cities along a given route.
+* Compute and print the total distance (the sum of all distances).
+* Find and print the smallest and greatest distance between two neighboring cities.
+* Find and print the mean distance between two neighboring cities.
+*/
 
 int main () {
-
-    cout << "enter nubmers or doubles, terminate by entering non-number:\n";
-
     vector<double> numbers;
 
+    // get input
     for (double temp; cin >> temp;) {
         numbers.push_back(temp);
     }
 
-    cout << "\nthe numbers user entered:\n";
-    for (double n : numbers ) {
+    // print the inputs
+    for (double n : numbers) {
         cout << n << "  ";
     }
 
-    ranges::sort(numbers);
-
-    if (numbers.size()%2 == 0) {
-        cout << "even\n";
-        double temp = numbers[numbers.size()/2] + numbers[(numbers.size()/2)-1];
-        temp /= 2;
-        cout << "median is " << temp;
-
-    } else {
-        cout << "oddn\n";
-        cout << "median is " << numbers[numbers.size()/2];
+    // get total distance
+    double total {};
+    for (int i = 0; i < numbers.size(); ++i) {
+        total += numbers.at(i);
     }
+    cout << "the total is " << total << "\n\n";
+
+    vector<double> distances;
+    for (int i = 0; i < numbers.size()-1; ++i) {
+            double tempDistance {};
+            tempDistance = abs(numbers.at(i) - numbers.at(i+1));
+            distances.push_back(tempDistance);
+    }
+
+    for (double d : distances) {
+        cout << d << "  ";
+    }
+    cout << "\n\n";
+
+    double smallest = distances.at(0);
+    double largest = distances.at(0);
+
+    for (int i = 0; i < distances.size(); ++i) {
+        if (distances.at(i) > largest) {
+            largest = distances.at(i);
+        }
+
+        if (distances.at(i) < smallest) {
+            smallest = distances.at(i);
+        }
+    }
+
+    cout << "smallest is " << smallest << "\n";
+    cout << "largest is " << largest << "\n";
 
 }
