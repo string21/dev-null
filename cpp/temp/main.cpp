779d59b9eaef9b00188627f72689b634e36f5d85
gdroid <gdroid0021@gmail.com>
2025-10-26 18:57:02 +0800

    build prime numbers

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void rewardChecker(int grainCountToCheck, vector<unsigned long long> grainPerSquare);
+int main() {
 
-int main () {
-    vector<unsigned long long> grainPerSquare = {};
-    unsigned long long grainCount = 1;
-    constexpr short chessSquares = 64;
+    vector<int> primes = {};
 
-    for (int i = 1; i <= chessSquares; ++i) {
-        grainPerSquare.push_back(grainCount);
-        grainCount*=2;
+    for(int i=2; i<=100; ++i) {
+
+        bool isPrime = true;
+
+        for(int p : primes) {
+            if(i % p == 0) { // the main logic
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primes.push_back(i);
+        }
+
     }
 
-    rewardChecker(1000, grainPerSquare);
-    rewardChecker(1000000, grainPerSquare);
-    rewardChecker(1000000000, grainPerSquare);
-}
-
-void rewardChecker(int grainCountToCheck, vector<unsigned long long> grainPerSquare) {
-    int accumulator = {};
-    for (int i = 0; i < grainPerSquare.size(); ++i) {
-        accumulator += grainPerSquare[i];
-        if (accumulator >= grainCountToCheck) {
-            cout << "it takes about " << i+1 << " squares to give the chess inventor " << grainCountToCheck << " grains of rice.\n";
-            cout << i+1 << " squares amounts to a total of " << accumulator << " grains of rice\n\n";
-            break;
-        }
+    for (int p : primes) {
+        cout << p << "  ";
     }
 }
