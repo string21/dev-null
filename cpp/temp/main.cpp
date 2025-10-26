8ca2af8556ed7ec43d4909dd01d581092a75e597
gdroid <gdroid0021@gmail.com>
2025-10-26 21:55:50 +0800

    get mode

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -2,37 +2,35 @@
 #include <vector>
 using namespace std;
 
+int getMax(vector<int> numSet);
+
 int main() {
+    vector<int> numSet = {
+        1,2,3,4,5,6,7,8,9,9,9
+    };
 
-    cout << "enter the number of primes to print: ";
-    int count = {};
-    cin >> count;
+    vector<int> counter(getMax(numSet)+1, 0); // this could be improved with map but not familiar with that yet
 
-    vector<int> primes;
-
-
-    for (int i=2; primes.size() < count; ++i) {
-        bool isPrime = true;
-
-        for (int p : primes) {
-            // this is optional, no need ot check past the sqrt of i here
-            if (p*p > i) break;
-
-            if (i % p == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime) {
-            primes.push_back(i);
-        }
-
-
+    for(int i = 0; i < numSet.size(); ++i) {
+        int temp = numSet[i];
+        counter[temp] += 1;
     }
 
-    for (int p : primes) {
-        cout << p << "  ";
+    for(int i = 0; i < counter.size(); i++) {
+        cout << i << " = " << counter[i];
+        if (counter[i] == getMax(counter)) { // compute getmax outside loop to improve performance
+            cout << " ---->> this is our mode, we have " << counter[i] << " instances of " << i;
+        }
+        cout << "\n";
     }
+}
 
+int getMax(vector<int> numSet) {
+    int max = numSet[0];
+    for(int n : numSet) {
+        if (n > max) {
+            max = n;
+        }
+    }
+    return max;
 }
