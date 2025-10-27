18e95dd6bb0d248f4c773b836df376ee307f899e
gdroid <gdroid0021@gmail.com>
2025-10-27 11:53:18 +0800

    solve quadratic equation

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,95 +1,31 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
 
-string getMin(vector<string> list);
-string getMax(vector<string> list);
-string getMode(vector<string> unique, vector<int> modeCounter);
+// solve quadratic equation --- use quadratic formula to find x, and then plug that into the equation to verify
 
 int main() {
+    double a = {};
+    double b = {};
+    double c = {};
 
-    vector<string> words = {
-        "xenon", "omega", "alpha", "beta", "gamma", "lambda", "sigma", "zeta", "alpha", "alpha",
-        "beta", "omega", "theta", "lambda", "lambda", "lambda", "gamma", "sigma", "zulu", "zulu", "zulu"
-    };
+    cout << "solve for the quadratic equation, provide a b and c (double values): ";
+    cin >> a >> b >> c;
+    cout << "\n";
 
+    // so we use here the quadratic formula to find x first (two solutions for x1 and x2)
+    double x1 = (-(b) + (sqrt((pow(b, 2)) - ((4*a)*c)))) / (2*a);
+    double x2 = (-(b) - (sqrt((pow(b, 2)) - ((4*a)*c)))) / (2*a);
+    cout << "x1 is " << x1 << "\n";
+    cout << "x2 is " << x2 << "\n";
 
+    // this is the quadratic equation
+    //ax2 + bx + c
+    x1 = (a*(pow(x1, 2))) + (b*x1) + c;
+    x2 = (a*(pow(x2, 2))) + (b*x2) + c;
 
-    vector<string> unique = {};
-
-    for(int i = 0; i < words.size(); ++i) {
-        bool isUnique = true;
-
-        for (string u : unique) {
-            if (u == words[i]) {
-                isUnique = false;
-            }
-        }
-
-        if (isUnique) {
-            unique.push_back(words[i]);
-        }
+    if (x1 == 0 && x2 == 0) {
+        cout << "valid inputs\n";
+    } else {
+        cout << "------------ >> invalid inputs aka no real roots in math\n";
     }
-
-    vector<int> modeCounter(unique.size(), 0);
-
-    for (int i = 0; i < words.size(); ++i) {
-        for (int j = 0; j < unique.size(); ++j) {
-            if (unique[j] == words[i]) {
-                ++modeCounter[j];
-            }
-        }
-    }
-
-
-
-    cout << "min (alphabetically) is " << getMin(words) << "\n";
-    cout << "max (alphabetically) is " << getMax(words) << "\n";
-    cout << "mode is " << getMode(unique, modeCounter) << "\n";
-
-}
-
-string getMode(vector<string> unique, vector<int> modeCounter) {
-    int max = modeCounter[0];
-    int maxIndex = 0;
-
-    // cout << "max is initialized to " << max << "\n";
-
-    for (int i = 0; i < modeCounter.size(); i++) {
-        //cout << "checking modeCounter index " << i << " -- " << modeCounter[i] << "\n";
-        if (max < modeCounter[i]) {
-            // cout << "max is being replaced..." << "\n";
-            max = modeCounter[i];
-            maxIndex = i;
-        }
-    }
-
-    // cout << "final max index is " << maxIndex << "\n";
-
-    for (int i = 0; i < modeCounter.size(); i++) {
-         // cout << unique[i] << " count is " << modeCounter[i] << "\n";
-        // if (modeCounter[i] == max) return unique[i];
-    }
-    return unique[maxIndex];
-}
-
-string getMin(vector<string> list) {
-    string min = list[0];
-    for (string l : list) {
-        if (l < min) {
-            min = l;
-        }
-    }
-    return min;
-}
-
-string getMax(vector<string> list) {
-    string max = list[0];
-    for (string l : list) {
-        if (l > max) {
-            max = l;
-        }
-    }
-    return max;
 }
