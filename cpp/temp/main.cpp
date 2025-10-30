73ca1894de948a258bfbf0d96dacc9e3c029d7d2
gdroid <gdroid0021@gmail.com>
2025-10-31 06:31:53 +0800

    puzzle ... have yet to understand lambdas,templates, etc...

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <functional>
 
 using namespace std;
 
 int area(int len, int wid);
-void expect(bool condition, string errMsg);
+bool validInputForAreaCheck(int len, int wid);
+
+
+void expect(function<bool()> condition, string errMsg) {
+    if (!condition)
+        throw runtime_error(errMsg);
+}
 
 int main() {
     try {
-        cout << area(2,0);
+        cout << area(2,2);
     } catch(runtime_error& e) {
         cerr << e.what();
     }
 }
 
+
 int area(int len, int wid) {
-    expect(len > 0 && wid > 0, "non-positive length or width");
+    expect([&]{ return len > 0 && wid > 0; }, "non-positive length or width");
     return len * wid;
 }
 
-void expect(bool condition, string errMsg) {
-    if (!condition)
-        throw runtime_error(errMsg);
+bool validInputForAreaCheck(int len, int wid) {
+    if (len > 0 && wid > 0) return true;
+    return false;
 }
+
