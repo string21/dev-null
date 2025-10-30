8f565fc349123ccfad08f247fb11d8e2316e6e87
gdroid <gdroid0021@gmail.com>
2025-10-31 04:09:02 +0800

    tried to implement expect function to separate precondition logic from main function logic to check my understanding of separation of concerns

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,40 +1,26 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
+
 using namespace std;
 
-void error(string s);
+int area(int len, int wid);
+void expect(bool condition, string errMsg);
 
 int main() {
     try {
-        int num = 0;
-        cout << "enter integer:  ";
-        cin >> num;
-
-        if (!cin) {
-            // cout << "no throw here...\n";
-            error("bad cin input...");
-        }
-
-        cout << "input is: " << num << "\n";
-
-        vector<int> numCollection;
-        numCollection.push_back(21);
-        numCollection.push_back(42);
-        cout << "first number is " << numCollection[0] << "\n";
-        cout << "second number is " << numCollection[1] << "\n";
-        // instead of relying on .at() to throw the out of range
-        // im going to manually test myself throw manually which gives me
-        // option to send a custom message
-        int temp = 2;
-        if (temp >= numCollection.size())
-            throw out_of_range{"out of bounds vector"};
-        cout << "third number is " << numCollection.at(temp) << "\n";
-
-    } catch (runtime_error& e) {
-        cerr << "INPUT ERROR: " << e.what() << "\n";
-    } catch (out_of_range& e) {
-        cerr << "RANGE ERROR: " << e.what() << "\n";
+        cout << area(2,0);
+    } catch(runtime_error& e) {
+        cerr << e.what();
     }
 }
-void error(string s) {
-    throw runtime_error(s);
+
+int area(int len, int wid) {
+    expect(len > 0 && wid > 0, "non-positive length or width");
+    return len * wid;
+}
+
+void expect(bool condition, string errMsg) {
+    if (!condition)
+        throw runtime_error(errMsg);
 }
