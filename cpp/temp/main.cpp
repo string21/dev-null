0da6ef1c6cc4df7abc2e67cb35f0985aac78d15c
gdroid <gdroid0021@gmail.com>
2025-10-31 08:11:22 +0800

    implemented expect design pattern using function call via template and lambda

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 
 // implement full try catch with user-defined expect design pattern
+// this time using the template plus lambda
+// i dont fully understand template and lamdas .. just testing if i can do this
 
 void error(string e);
 int area(int length, int width);
-void expect(bool preconditionCheck, string eMsg);
+template<typename Whatever>
+void expect(Whatever preconditionCheck, string someErrorMessgae) {
+    if (!preconditionCheck()) error(someErrorMessgae);
+}
+
+
 
 int main() {
     try {
-        cout << area(0, 2);
+        cout << area(0, 99);
     } catch (runtime_error& e) {
         cerr << "ERROR: " << e.what() << "\n";
     }
 }
 
 int area(int length, int width) {
-    expect(length > 0 && width > 0, "non-positive length or width...");
+    expect([&](){ return length > 0 && width > 0; }, "non-positive length or width...");
     return length * width;
 }
 
-void expect(bool preconditionCheck, string eMsg) {
-    if (!preconditionCheck) error(eMsg);
-}
 
 void error(string e) {
     throw runtime_error {e};
 }
+
