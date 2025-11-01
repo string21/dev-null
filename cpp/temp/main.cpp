f20a5bf1bee04ce24dc0d924f643aee98103dfb6
gdroid <gdroid0021@gmail.com>
2025-11-01 20:55:37 +0800

    some reorg update

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -12,8 +12,34 @@
 
 void welcomeMsg();
 std::vector<double> getUserInputs();
-void error(std::string e);
+void printResult(std::vector<double> inputs);
 void expect(bool pre_or_postcondition, std::string e);
+void error(std::string e);
+
+int main () {
+    try {
+        welcomeMsg();
+        std::vector<double> inputs = getUserInputs();
+        printResult(inputs);
+    } catch (std::exception& e) { // catch all types of exceptions, not just runtime_error
+        std::cerr << "ERROR: " << e.what();
+    } catch (...) {
+        std::cerr << "Oppps... Unknown Error Found";
+    }
+}
+
+void welcomeMsg() {
+    std::cout << "\nlet's solve some quadratic equations!\n"
+              << "please supply us with variables a, b and c to start.\n"
+              << "type them now and make sure to separate them using space:  ";
+}
+
+std::vector<double> getUserInputs() {
+    double a, b, c = {};
+    std::cin >> a >> b >> c;
+    expect(std::cin.good(), "Invalid User Input");
+    return {a, b, c};
+}
 
 // this is a bigger function just to finish this...
 void printResult(std::vector<double> inputs) {
@@ -32,35 +58,10 @@ void printResult(std::vector<double> inputs) {
               << "x2 is " << x2 << ", plugged into the quadratic equation: " << verify_x2 << "\n";
 }
 
-int main () {
-    try {
-        welcomeMsg();
-        std::vector<double> inputs = getUserInputs();
-        printResult(inputs);
-    } catch (std::exception& e) { // catch all types of exceptions, not just runtime_error
-        std::cerr << "ERROR: " << e.what();
-    } catch (...) {
-        std::cerr << "Oppps... Unknown Error Found";
-    }
-}
-
-std::vector<double> getUserInputs() {
-    double a, b, c = {};
-    std::cin >> a >> b >> c;
-    expect(std::cin.good(), "Invalid User Input");
-    return {a, b, c};
-}
-
-void welcomeMsg() {
-    std::cout << "\nlet's solve some quadratic equations!\n"
-              << "please supply us with variables a, b and c to start.\n"
-              << "type them now and make sure to separate them using space:  ";
+void expect(bool pre_or_postcondition, std::string e) {
+    if (!pre_or_postcondition) error(e);
 }
 
 void error(std::string e) {
     throw std::runtime_error {e};
-}
-
-void expect(bool pre_or_postcondition, std::string e) {
-    if (!pre_or_postcondition) error(e);
 }
