5e1cc3be54d9f8ec45c1c65cf19ac250fdfb6bd8
gdroid <gdroid0021@gmail.com>
2025-11-01 13:49:32 +0800

    quadratic equation with exception handling

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,77 +1,66 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
+#include <vector>
+#include <exception>
 
-// Formulas:
-// Celsius to Fahrenheit: $F = (C \times \frac{9}{5}) + 32$
-// Fahrenheit to Celsius: $C = (F - 32) \times \frac{5}{9}$
+// ill start using std:: instead of relying on "using namespace std" to avoid polluting the global namespace
 
-// ill make sure to break things into smaller functions and try to handle all legal and illegal inputs
+// quad formulas:
+    // x1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
+    // x2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
+// quad equation: (a * x * x) + (b * x) + c
 
-void printMenu();
-short getUserMenuChoice();
-void processChoice(short choice);
-void expect (bool validityCheck, string e);
-void error(string e);
+void welcomeMsg();
+std::vector<double> getUserInputs();
+void error(std::string e);
+void expect(bool pre_or_postcondition, std::string e);
 
-int main() {
+// this is a bigger function just to finish this...
+void printResult(std::vector<double> inputs) {
+    double a = inputs[0];
+    double b = inputs[1];
+    double c = inputs[2];
+
+    expect((b*b - 4*a*c) > 0, "MATH: No Real Roots");
+    double x1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
+    double x2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
+
+    double verify_x1 = (a * x1 * x1) + (b * x1) + c;
+    double verify_x2 = (a * x2 * x2) + (b * x2) + c;
+
+    std::cout << "x1 is " << x1 << ", plugged into the quadratic equation: " << verify_x1 << "\n"
+              << "x2 is " << x2 << ", plugged into the quadratic equation: " << verify_x2 << "\n";
+}
+
+int main () {
     try {
-        printMenu();
-        short choice = getUserMenuChoice();
-        processChoice(choice);
-    } catch (runtime_error& e) {
-        cerr << "ERROR:  " << e.what();
+        welcomeMsg();
+        std::vector<double> inputs = getUserInputs();
+        printResult(inputs);
+    } catch (std::exception& e) { // catch all types of exceptions, not just runtime_error
+        std::cerr << "ERROR: " << e.what();
     } catch (...) {
-        cout << "Opps...... unknown error!";
+        std::cerr << "Oppps... Unknown Error Found";
     }
 }
 
-int getTemperature() {
-    cout << "enter the temperature: ";
-    int temp = 0;
-    cin >> temp;
-    expect(cin.good(), "invalid temperature value...");
-    return temp;
+std::vector<double> getUserInputs() {
+    double a, b, c = {};
+    std::cin >> a >> b >> c;
+    expect(std::cin.good(), "Invalid User Input");
+    return {a, b, c};
 }
 
-void print_c_to_fahrenheit(double temp) {
-    cout << ((temp * 9) / 5) + 32;
+void welcomeMsg() {
+    std::cout << "\nlet's solve some quadratic equations!\n"
+              << "please supply us with variables a, b and c to start.\n"
+              << "type them now and make sure to separate them using space:  ";
 }
 
-void print_f_to_celsius(double temp) {
-    cout << ((temp - 32) * 5)/9;
+void error(std::string e) {
+    throw std::runtime_error {e};
 }
 
-void processChoice(short choice) {
-    int temperature = getTemperature();
-    switch (choice) {
-        case 1:
-            print_c_to_fahrenheit(temperature);
-            break;
-        case 2:
-            print_f_to_celsius(temperature);
-            break;
-    }
-}
-
-short getUserMenuChoice() {
-    short choice = 0;
-    cin >> choice;
-    expect(cin.good() && choice >= 1 && choice <= 2, "invalid user choice...");
-    return choice;
-}
-
-void printMenu() {
-    cout << "\n*** choose what you want to do today ***\n"
-         << "1. convert to fahrenheit \n"
-         << "2. convert to celsius \n"
-         << "enter your choice:  ";
-}
-
-void error(string e) {
-    throw runtime_error {e};
-}
-
-void expect (bool validityCheck, string e) {
-    // test if the function argument(s) are any good
-    if (!validityCheck) error(e);
+void expect(bool pre_or_postcondition, std::string e) {
+    if (!pre_or_postcondition) error(e);
 }
