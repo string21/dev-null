148807771ee60c59ef0aae1f1d1a541395431beb
gdroid <gdroid0021@gmail.com>
2025-11-03 09:44:21 +0800

    mods to cows and bulls

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -48,6 +48,7 @@ int main() {
         }
     } while (bull_count != 4);
 }
+
 int get_bull_count(std::vector<int> number_to_be_guessed, std::vector<int> user_input) {
     int bull_counter {};
     for (int i = 0; i < number_to_be_guessed.size(); ++i) {
@@ -56,6 +57,7 @@ int get_bull_count(std::vector<int> number_to_be_guessed, std::vector<int> user_
     }
     return bull_counter;
 }
+
 int get_cow_count(std::vector<int> number_to_be_guessed, std::vector<int> user_input) {
     int cow_counter {};
     for (int i = 0; i < number_to_be_guessed.size(); ++i) {
@@ -67,6 +69,7 @@ int get_cow_count(std::vector<int> number_to_be_guessed, std::vector<int> user_i
     }
     return cow_counter;
 }
+
 std::vector<int> get_user_input() {
     std::cout << "Enter 4 numbers (e.g. 4352) to guess:  " << std::endl;
     std::string userInput {};
@@ -81,6 +84,7 @@ std::vector<int> get_user_input() {
     expect(validInputs.size() == 4, "invalid user input");
     return validInputs;
 }
+
 std::vector<int> generateRandomSet() {
     std::vector<int> rando_set;
     while (rando_set.size() < 4) {
@@ -96,18 +100,22 @@ std::vector<int> generateRandomSet() {
     }
     return rando_set;
 }
+
 void print_welcome_msg() {
     std::cout << "*** Let's play Bulls and Cows! ***\n";
 }
+
 int random_int(int a, int b) {
     static unsigned seed = time(nullptr);
     static std::default_random_engine theEngine(seed);
     std::uniform_int_distribution<int> theDistribution(a, b);
     return theDistribution(theEngine);
 }
+
 void error(std::string e) {
     throw std::runtime_error {e};
 }
+
 void expect(bool correctExpectation, std::string e) {
     if (!correctExpectation) error(e);
 }
