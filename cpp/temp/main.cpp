c5fb60ff9f0783ae2db3ba45796f2ffdfc819485
gdroid <gdroid0021@gmail.com>
2025-11-05 10:47:14 +0800

    modified v2

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -7,49 +7,46 @@ void error(string e) {
 
 int main()
 {
-    cout << "Please enter expression (we can handle +, −,  *, and /\n";
-    cout << "add an x to end expression (e.g., 1+2*3x): ";
     int lval = 0;
     int rval = 0;
-    cin>>lval; // read left-most operand
-    if (!cin)
-        error("no first operand");
 
-    for (char op; cin>>op;) { // read operator and right-hand op
+    cout << "Please enter expression (we can handle +, −,  *, and /\n";
+    cout << "add an x to end expression (e.g., 1+2*3x): ";
+
+    cin>>lval; // read left-most operand
+    if (!cin) error("no first operand");
+
+    for (char op; cin>>op;) {
+
         cout << "operator captured is " << op << "\n";
-        if (op!='x')
-            cin>>rval;
-        else {
-            cout << "we received the x request, show the result...";
+
+        if (op=='x') {
+            cout << "Result: " << lval << '\n';
+            return 0; // they chose x to exit clean per user instructions
+        } else  {
+            cin >> rval; // read right-hand operand
+
+            if (cin.good()) cout << "cin is good\n";
+            if (!cin.good()) cout << "cin is bad\n";
+
+            if (!cin) error("no second operand");
         }
 
-        if (cin.good()) cout << "cin is good\n";
-        if (!cin.good()) cout << "cin is bad\n";
-
-        if (!cin)
-            error("no second operand");
-
-        if (op == 'x') {
-            cout << "Result: " << lval << '\n';
-            return 0;
-        } else {
-            switch(op){
-                case '+':
-                    lval += rval; // add: lval = lval + rval
-                    break;
-                case '-':
-                    lval -= rval; // subtract: lval = lval - rval
-                    break;
-                case '*':
-                    lval  *= rval; // multiply: lval = lval * rval
-                    break;
-                case '/':
-                    lval /= rval; // divide: lval = lval / rval
-                    break;
-                default:
-                    error("invalid op\n");
-
-            }
+        switch(op){
+            case '+':
+                lval += rval; // add: lval = lval + rval
+                break;
+            case '-':
+                lval -= rval; // subtract: lval = lval - rval
+                break;
+            case '*':
+                lval  *= rval; // multiply: lval = lval * rval
+                break;
+            case '/':
+                lval /= rval; // divide: lval = lval / rval
+                break;
+            default:
+                error("invalid operator\n");
         }
     }
     error("bad expression");
