6074922bdb4643d85ebbafbfc164377dcd2a7419
gdroid <gdroid0021@gmail.com>
2025-11-06 08:40:19 +0800

    testing constructors and the more efficient initializer list syntax

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -6,6 +6,19 @@ class Car {
 public:
     std::string make;
     std::string model;
+
+    Car(){}
+    Car(std::string m): make{m}, model{"1 param test"}{}
+    Car(std::string m, std::string m2): make{m}, model{m2} {
+        if (make == "Honda")
+            make = "Honda for real tho?";
+    }
+
+
+    void printCar() {
+        std::cout << "Make: " << make << "\n";
+        std::cout << "Model: " << model << "\n";
+    }
 };
 
 int main () {
@@ -13,7 +26,13 @@ int main () {
     Car someCar;
     someCar.make = "Honda";
     someCar.model = "Civic";
-
     std::cout << "My car is not a " << someCar.make << " " << someCar.model << std::endl;
+    someCar.printCar();
+
+    Car car1param("Ford");
+    car1param.printCar();
+
+    Car carWithTwoParams("Honda", "Accord");
+    carWithTwoParams.printCar();
 
 }
