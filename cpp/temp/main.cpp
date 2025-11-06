ec1f39d35d7f225961fe5bb5aaa45c080e74c4c0
gdroid <gdroid0021@gmail.com>
2025-11-06 08:31:08 +0800

    testing class

--- cpp/temp/main.cpp
+++ cpp/temp/main.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
-using namespace std;
 
-void error(string e) {
-    throw runtime_error {e};
-}
+// im trying to learn class real quick so i can understand this token concept being presented by bjarn
 
-int main()
-{
-    int lval = 0;
-    int rval = 0;
+class Car {
+public:
+    std::string make;
+    std::string model;
+};
 
-    cout << "Please enter expression (we can handle +, −,  *, and /\n";
-    cout << "add an x to end expression (e.g., 1+2*3x): ";
+int main () {
 
-    cin>>lval; // read left-most operand
-    if (!cin) error("no first operand");
+    Car someCar;
+    someCar.make = "Honda";
+    someCar.model = "Civic";
 
-    for (char op; cin>>op;) {
+    std::cout << "My car is not a " << someCar.make << " " << someCar.model << std::endl;
 
-        cout << "operator captured is " << op << "\n";
-
-        if (op=='x') {
-            cout << "Result: " << lval << '\n';
-            return 0; // they chose x to exit clean per user instructions
-        } else  {
-            cin >> rval; // read right-hand operand
-
-            if (cin.good()) cout << "cin is good\n";
-            if (!cin.good()) cout << "cin is bad\n";
-
-            if (!cin) error("no second operand");
-        }
-
-        switch(op){
-            case '+':
-                lval += rval; // add: lval = lval + rval
-                break;
-            case '-':
-                lval -= rval; // subtract: lval = lval - rval
-                break;
-            case '*':
-                lval  *= rval; // multiply: lval = lval * rval
-                break;
-            case '/':
-                lval /= rval; // divide: lval = lval / rval
-                break;
-            default:
-                error("invalid operator\n");
-        }
-    }
-    error("bad expression");
 }
